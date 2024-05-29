90f254ad8c98590db473522387c455b9a128d82d
Utkarsh-Kumar-Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-05-30 00:27:59 +0530

    Added Education and project section in nav bar

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ const observer = new IntersectionObserver((entries) => {
 revealElements.forEach((element) => {
     observer.observe(element);
 });
+
+// for clicking the project button
+const projectbut=document.getElementById('projectbutton');
+const secondcontainer=document.getElementById('second');
+
+// for clicking the education button 
+const educationbut=document.getElementById('educationbutton');
+const edusection=document.getElementById('education-section');
+projectbut.addEventListener('click',()=>{
+    if(secondcontainer.style.display=='none' ||secondcontainer.style.display==''){
+        secondcontainer.style.display='block';
+        edusection.style.display='none';
+    }
+    else{
+        secondcontainer.style.display='none';
+    }
+
+})
+
+
+educationbut.addEventListener('click',()=>{
+    if(edusection.style.display=='none' ||edusection.style.display==''){
+        edusection.style.display='flex';
+        secondcontainer.style.display='none';
+        
+    }
+    else{
+        edusection.style.display='none';
+    }
+
+})
